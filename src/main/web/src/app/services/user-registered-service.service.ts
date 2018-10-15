0f5930685b725430b623921f0c804c7c7533fa55
Maciej Bihun <maciej.bihun@hpe.com>
2018-10-15 11:20:16 +0200

    Update user-registered-services url

--- src/main/web/src/app/services/user-registered-service.service.ts
+++ src/main/web/src/app/services/user-registered-service.service.ts
@@ -6,6 +6,8 @@ import {Observable} from 'rxjs/index';
 @Injectable()
 export class UserRegisteredServiceService {
 
+    private readonly USER_REGISTERED_SERVICES: string = 'user-registered-services';
+
     constructor(private http: HttpClient) {
     }
 
@@ -33,12 +35,12 @@ export class UserRegisteredServiceService {
     }*/
 
     public getUserRegisteredServices(): Observable<Array<UserRegisteredService>> {
-      return this.http.get<Array<UserRegisteredService>>('/user-registered-services');
+      return this.http.get<Array<UserRegisteredService>>(this.USER_REGISTERED_SERVICES);
     }
 
     public saveUserRegisteredService(userRegisteredService: UserRegisteredService){
       const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-      return this.http.post<UserRegisteredService>('/register-service',
+      return this.http.post<UserRegisteredService>(this.USER_REGISTERED_SERVICES,
         JSON.stringify(userRegisteredService), {headers: headers});
     }
 
