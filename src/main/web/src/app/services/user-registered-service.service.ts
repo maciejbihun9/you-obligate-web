38b4a28a76b7df4e9658f752811460294f187769
Maciej Bihun <maciej.bihun@hpe.com>
2018-10-15 11:20:36 +0200

    Remove unused code

--- src/main/web/src/app/services/user-registered-service.service.ts
+++ src/main/web/src/app/services/user-registered-service.service.ts
@@ -11,29 +11,6 @@ export class UserRegisteredServiceService {
     constructor(private http: HttpClient) {
     }
 
-
-    /*public getForecast(financialPeriodId, rootSubRegionId) {
-        return this.http.get('/api/Forecast/', {
-            params: {
-                'financialPeriodId': financialPeriodId,
-                'subRegionId': rootSubRegionId
-            }
-        }).pipe(
-            tap(forecastModel => {console.log(`ForecastModel has been parsed for props: ${financialPeriodId}, ${rootSubRegionId}`); })
-        );
-    }*/
-
-    /**
-     *
-     * @returns {Observable<Array<ForecastMetrics>>} -> [{FiscalYear: 2019, ForecastPeriodId: 352, PeriodName: "Q1", TCV: 1.23, ABR: 2.31, FFYR: 3.12}]
-     */
-    /*public getForecastMetrics(forecastModel: Forecast): Observable<Array<ForecastMetrics>> {
-        const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-        forecastModel.revenueMeasureId = 2; // this is just default value without any effect
-        return this.http.post<Array<ForecastMetrics>>('/api/Forecast/GetForecastMetrics',
-            JSON.stringify(forecastModel), {headers: headers});
-    }*/
-
     public getUserRegisteredServices(): Observable<Array<UserRegisteredService>> {
       return this.http.get<Array<UserRegisteredService>>(this.USER_REGISTERED_SERVICES);
     }
