612f42071f02c3cbb18210d657e4d1087134707e
Maciej Bihun <maciej.bihun@hpe.com>
2018-10-16 14:17:34 +0200

    Webclient get method added

--- src/main/web/src/app/services/user-registered-service.service.ts
+++ src/main/web/src/app/services/user-registered-service.service.ts
@@ -6,18 +6,21 @@ import {Observable} from 'rxjs/index';
 @Injectable()
 export class UserRegisteredServiceService {
 
-    private readonly USER_REGISTERED_SERVICES: string = 'user-registered-services';
+    private readonly USER_REGISTERED_SERVICES_URL: string = '/user-registered-services';
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient) {}
+
+    public getUserRegisteredService(userRegisteredServiceId: number): Observable<UserRegisteredService> {
+      return this.http.get<UserRegisteredService>(this.USER_REGISTERED_SERVICES_URL + '/${userRegisteredServiceId}');
     }
 
     public getUserRegisteredServices(): Observable<Array<UserRegisteredService>> {
-      return this.http.get<Array<UserRegisteredService>>(this.USER_REGISTERED_SERVICES);
+      return this.http.get<Array<UserRegisteredService>>(this.USER_REGISTERED_SERVICES_URL);
     }
 
     public saveUserRegisteredService(userRegisteredService: UserRegisteredService){
       const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-      return this.http.post<UserRegisteredService>(this.USER_REGISTERED_SERVICES,
+      return this.http.post<UserRegisteredService>(this.USER_REGISTERED_SERVICES_URL,
         JSON.stringify(userRegisteredService), {headers: headers});
     }
 
