b1c2dfb7a238a887080bfdbab99c94a96f97fc9e
Bihun <XBBNWYJ@eur.bnymellon.net>
2019-02-05 15:58:03 +0100

    Add status to group join request model

--- src/main/web/src/app/models/group-join-request.model.ts
+++ src/main/web/src/app/models/group-join-request.model.ts
@@ -3,12 +3,11 @@ import {UserRegisteredService} from "./user-registered-service.model";
 import {ObligationGroup} from "./obligation-group.model";
 
 export class GroupJoinRequest {
-
   id?;
   user: User;
   obligationGroup: ObligationGroup;
   userRegisteredService: UserRegisteredService;
   proposedUnitOfWorkCost: number;
   proposedUnitOfWorkType: string;
-
+  status: string;
 }
