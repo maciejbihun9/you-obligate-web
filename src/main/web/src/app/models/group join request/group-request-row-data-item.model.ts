ac5d6de6524d78eca3c513910a703fe4bdeb9bb2
Bihun <XBBNWYJ@eur.bnymellon.net>
2019-02-05 16:15:32 +0100

    Extend GroupRequestRowDataItem

--- src/main/web/src/app/models/group join request/group-request-row-data-item.model.ts
+++ src/main/web/src/app/models/group join request/group-request-row-data-item.model.ts
@@ -1,8 +1,17 @@
 
 export class GroupRequestRowDataItem {
 
+  // user data
+  name: string;
+  surname: string;
   username: string;
+  contactNumber: string;
+  // user registered service model data
   userRegisteredServiceName: string;
+  userRegisteredServiceCategory: string;
+  serviceDescription: string;
+  experienceDescription: string;
+  // group join request data
   proposedUnitOfWorkType: string;
   proposedUnitOfWorkCost: number;
 
