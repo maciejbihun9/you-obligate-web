c720ff6788d5e1e09ec9d047e29df5185fb67573
Maciej Bihun <maciej.bihun@hpe.com>
2019-02-07 14:46:40 +0100

    Remove cache comment section

--- src/main/web/src/app/components/obligation-groups-view/obligation-groups-view.component.ts
+++ src/main/web/src/app/components/obligation-groups-view/obligation-groups-view.component.ts
@@ -15,44 +15,14 @@ export class ObligationGroupsViewComponent implements OnInit {
 
   public obligationGroups: Array<ObligationGroup>;
 
-  private obligationGroupsCacheInterval;
-
-  private updateCacheTimeLeft = 600;
-
   constructor(private obligationGroupsService: ObligationGroupsService) { }
 
   ngOnInit() {
-    // fetch obligation groups data from cache is it was set before
-      // if obligation groups cache is not set then make a http call to poll for it
     this.obligationGroupsService.getObligationGroups().subscribe(
       obligationGroups => {
-        /*ObligationGroupsService.obligationGroupsCache = new Map();
-        obligationGroups.forEach(obligationGroup => {
-          ObligationGroupsService.obligationGroupsCache.set(obligationGroup.id, obligationGroup);
-        });*/
         this.obligationGroups = obligationGroups;
       }
     );
-    // start cache timer
-    //this.updateCache();
   }
 
-  /*public updateCache() {
-    this.obligationGroupsCacheInterval = setInterval(() => {
-      if (this.updateCacheTimeLeft > 0) {
-        this.updateCacheTimeLeft--;
-      } else {
-        this.updateCacheTimeLeft = 600;
-        // update cache
-        // get all cache keys
-        this.obligationGroupsService.getObligationGroupsWithBonds(ObligationGroupsService.obligationGroupsCache.keys()).subscribe(
-          obligationGroupsWithBonds => {
-            obligationGroupsWithBonds.forEach(obligationGroupWithBonds => {
-              ObligationGroupsService.obligationGroupsCache.set(obligationGroupWithBonds.id, obligationGroupWithBonds);
-            });
-          }
-        );
-      }
-    }, 1000);
-  }*/
 }
