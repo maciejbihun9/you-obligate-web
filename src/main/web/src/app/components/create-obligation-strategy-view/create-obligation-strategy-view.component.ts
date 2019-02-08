adc344fbd73f9e0c7aa06c47cf6a1233ab8ae352
Bihun <XBBNWYJ@eur.bnymellon.net>
2019-02-08 17:02:52 +0100

    Make unit of work properties updateable

--- src/main/web/src/app/components/create-obligation-strategy-view/create-obligation-strategy-view.component.ts
+++ src/main/web/src/app/components/create-obligation-strategy-view/create-obligation-strategy-view.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {GroupJoinRequestService} from "../../services/group-join-request.service";
-import {GroupJoinRequest} from "../../models/group join request/group-join-request.model";
+import {GroupJoinRequest} from "../../models/group-join-request/group-join-request.model";
+import {UnitOfWork} from "../../models/unit-of-work.model";
 
 @Component({
   selector: 'app-create-obligation-strategy-view',
@@ -14,11 +15,22 @@ export class CreateObligationStrategyViewComponent implements OnInit {
 
   groupJoinRequest: GroupJoinRequest;
 
+  unitOfWorkCost: number;
+
+  unitOfWorkTypes = Object.keys(UnitOfWork).filter(unitOfWork => !isNaN(UnitOfWork[unitOfWork]));
+
+  unitOfWorkType: UnitOfWork;
+
   constructor(private groupJoinRequestService: GroupJoinRequestService) { }
 
   ngOnInit() {
+
     this.groupJoinRequestService.getGroupJoinRequest(this.groupJoinRequestId).subscribe(groupJoinRequest => {
       this.groupJoinRequest = groupJoinRequest;
+      debugger;
+      // init unit of work items with user proposed data
+      this.unitOfWorkCost = groupJoinRequest.proposedUnitOfWorkCost;
+      this.unitOfWorkType = groupJoinRequest.proposedUnitOfWorkType;
     });
   }
 
