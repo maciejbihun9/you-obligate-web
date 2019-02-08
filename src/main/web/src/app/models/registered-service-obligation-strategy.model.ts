7c18ad069144057558fb4f343db31f78437c17d6
Bihun <XBBNWYJ@eur.bnymellon.net>
2019-02-08 12:39:10 +0100

    Update user-obligation-groups-panel component to reflect changes made in component and a model

--- src/main/web/src/app/models/registered-service-obligation-strategy.model.ts
+++ src/main/web/src/app/models/registered-service-obligation-strategy.model.ts
@@ -1,8 +1,11 @@
 import {UserRegisteredService} from './user-registered-service.model';
 import {UserAccountInObligationGroup} from './user-account-in-obligation-group.model';
+import {UnitOfWork} from "./unit-of-work.model";
 
 export interface RegisteredServiceObligationStrategy {
   id: number;
   userRegisteredService: UserRegisteredService;
   userAccountInObligationGroup: UserAccountInObligationGroup;
+  unitOfWork: UnitOfWork;
+  unitOfWorkCost: number;
 }
